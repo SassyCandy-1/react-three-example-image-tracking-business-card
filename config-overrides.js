3edda72feb343159a67df675c5665145855e7e7e
SassyCandy-1 <67623872+SassyCandy-1@users.noreply.github.com>
2023-07-16 11:55:36 +0800

    changes

--- config-overrides.js
+++ config-overrides.js
@@ -1,28 +1,33 @@
 module.exports = {
   webpack: function (config, env) {
-    config.module.rules = config.module.rules.map(rule => {
+    config.module.rules = config.module.rules.map((rule) => {
       if (rule.oneOf instanceof Array) {
-        rule.oneOf[rule.oneOf.length - 1].exclude = [/\.(js|mjs|zbin|jsx|ts|tsx)$/, /\.html$/, /\.json$/];
+        rule.oneOf[rule.oneOf.length - 1].exclude = [
+          /\.(cjs|js|mjs|zbin|jsx|ts|tsx)$/,
+          /\.html$/,
+          /\.json$/,
+        ];
         return {
           ...rule,
-          oneOf: [{
+          oneOf: [
+            {
               test: /zcv\.wasm$/,
               type: "javascript/auto",
               loader: "file-loader",
               options: {
-                outputPath: 'static/js',
-                publicPath: '.',
-                name: '[name].[ext]',
+                outputPath: "static/js",
+                publicPath: ".",
+                name: "[name].[ext]",
               },
             },
-            ...rule.oneOf
-          ]
+            ...rule.oneOf,
+          ],
         };
       }
       return rule;
     });
 
-    config.resolve.extensions.push(".wasm")
+    config.resolve.extensions.push(".wasm");
     return config;
   },
   jest: function (config) {
@@ -31,11 +36,11 @@ module.exports = {
   devServer: function (configFunction) {
     return function (proxy, allowedHost) {
       const config = configFunction(proxy, allowedHost);
-      config.https = true
+      config.https = true;
       return config;
     };
   },
   paths: function (paths, env) {
     return paths;
   },
-}
+};
